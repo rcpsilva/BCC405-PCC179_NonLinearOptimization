ae9c33c721dc717bc983957a9bff0c4e39a0d75d
rcpsilva <rcpsilva@gmail.com>
2023-11-22 14:58:13 -0300

    Update visualize.py

--- 2023-2/SourceCode/preclass/visualize.py
+++ 2023-2/SourceCode/preclass/visualize.py
@@ -31,8 +31,8 @@ if __name__ == '__main__':
     #f = lambda x: x[0] - x[1] + 2*x[0]**2 + 2*x[0]*x[1] + x[1]**2
     #g = lambda x: np.array([1 + 4*x[0] + 2*x[1], -1 + 2*x[0] + 2*x[1]])
 
-    f = lambda x: 10*x[0]**2 + 2*x[1]**2
-    g = lambda x: np.array([20*x[0], 4*x[1]])
+    f = lambda x: 12*x[0]**2 + 2*x[1]**2
+    g = lambda x: np.array([24*x[0], 4*x[1]])
 
     #f = lambda x: 0.1*x[0]**2 + 2*x[1]**2
     #g = lambda x: np.array([0.2*x[0], 4*x[1]])
@@ -44,9 +44,9 @@ if __name__ == '__main__':
 
     _, gd1, _, _ = gradient_descent(x0,f,g,step_size=0.01,niter=1000,tol=1e-6)
     _, gd2, _, _ = gradient_descent(x0,f,g,step_size=0.07,niter=1000,tol=1e-6)
-    _, gdm, _ = gd_momentum(x0,f,g,niter=500,tol=1e-6,eta=0.07,beta=0.3) 
-    _, rms, _  = rmsprop(x0,f,g,niter=500,tol=1e-6,gamma=0.9,eta=0.9)
-    _, gda, _, _ = gradient_descent_adaptive_step(x0,f,g,step_size=2,niter=1000,tol=1e-6)
+    _, gdm, _ = gd_momentum(x0,f,g,niter=500,tol=1e-6,eta=0.05,beta=0.3) 
+    _, rms, _  = rmsprop(x0,f,g,niter=500,tol=1e-6,gamma=0.9,eta=0.95)
+    _, gda, _, _ = gradient_descent_adaptive_step(x0,f,g,step_size=0.95,niter=1000,tol=1e-6)
 
     
     #plot_sequence(f,gd1,[-5,-5],[5,5],0.1,30)
@@ -54,7 +54,8 @@ if __name__ == '__main__':
     plot_sequence(f,gdm,[-5,-5],[5,5],0.1,30)
     plot_sequence(f,rms,[-5,-5],[5,5],0.1,30)
     #plot_sequence(f,gda,[-5,-5],[5,5],0.1,30)
-    plt.legend()
+
+    print(f'niter:\n gd1 {len(gd1)}\n gd2 {len(gd2)} \n gdm {len(gdm)} \n rms {len(rms)} \n gda {len(gda)}')
     plt.show()
     #plt.plot(ys)
     #plt.show()
