7a5d32eb5c79acb81c2542fcd8b7f370db3f9d32
rcpsilva <rcpsilva@gmail.com>
2023-11-22 19:05:55 -0300

    updated optimize

--- 2023-2/SourceCode/optimize.py
+++ 2023-2/SourceCode/optimize.py
@@ -1,5 +1,76 @@
 import numpy as np 
 
+def adam(x0,f,grad,niter=500,tol=1e-6,eta=0.9,beta1=0.9,beta2=0.99):
+
+    epsilon = 1e-8
+    x = x0
+    iter = 1
+    xs = [x0]
+    ys = [f(x0)]
+    s = np.zeros(len(x0))
+    v = np.zeros(len(x0))
+
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        g = grad(x)
+        
+        v = beta1*v + (1-beta1)*g 
+        s = beta2*s + (1-beta2)*g**2
+        vt = v/(1-beta1**iter)
+        st = s/(1-beta2**iter)
+
+        d = - eta/(np.sqrt(st+epsilon))*vt
+
+        x = x + d
+        
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} grad:{g}')
+        iter+=1
+        
+    return x, xs, ys
+
+
+def rmsprop(x0,f,grad,niter=500,tol=1e-6,eta=0.9,gamma=0.98):
+
+    epsilon = 1e-8
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    s = np.zeros(len(x0))
+
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        g = grad(x)
+        s = gamma*s + (1-gamma)*g**2
+        x = x - eta/np.sqrt(s+epsilon)*g
+        
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} grad:{g}')
+        iter+=1
+        
+    return x, xs, ys
+
+def gradient_descent_momentum(x0,f,grad,niter=500,tol=1e-6,eta=0.9,beta=0.98):
+
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    v = np.zeros(len(x0))
+
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        g = grad(x)
+        v = beta*v + g
+        x = x - eta * v
+        
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} grad:{g}')
+        iter+=1
+        
+    return x, xs, ys
+
 def gradient_descent(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
 
     x = x0
--- 2023-2/SourceCode/visualize.py
+++ 2023-2/SourceCode/visualize.py
@@ -17,7 +17,7 @@ def plot_sequence(fun,seq,lb,up,delta,levels):
     plt.contour(X, Y, Z,levels=levels)
     p0 = [p[0] for p in seq]
     p1 = [p[1] for p in seq]
-    plt.plot(p1,p0,'-')
+    plt.plot(p1,p0,'-d')
 
     #for p in seq:
     #    plt.plot(p[0],p[1],'r.')
@@ -26,7 +26,7 @@ def plot_sequence(fun,seq,lb,up,delta,levels):
 
 if __name__ == '__main__':
 
-    from optimize import gradient_descent, gradient_descent_adaptive_step
+    from optimize import gradient_descent, gradient_descent_adaptive_step, gradient_descent_momentum, rmsprop, adam
 
     #f = lambda x: x[0] - x[1] + 2*x[0]**2 + 2*x[0]*x[1] + x[1]**2
     #g = lambda x: np.array([1 + 4*x[0] + 2*x[1], -1 + 2*x[0] + 2*x[1]])
@@ -37,17 +37,27 @@ if __name__ == '__main__':
     #f = lambda x: 2*x[0]**2 + x[1]**2 + 2*x[0]*x[1]
     #g = lambda x: np.array([4*x[0] + 2*x[1], 2*x[1] + 2*x[0]])
 
-    x0 = np.array([-4,-4])
+    x0 = np.array([-3,-4])
 
     _, gd1, _, _ = gradient_descent(x0,f,g,step_size=0.01,niter=1000,tol=1e-6)
     _, gd2, _, _ = gradient_descent(x0,f,g,step_size=0.09,niter=1000,tol=1e-6) 
-    _, gda, _, _ = gradient_descent_adaptive_step(x0,f,g,step_size=2,niter=1000,tol=1e-6)
-
+    _, gdm, _ = gradient_descent_momentum(x0,f,g,niter=1000,tol=1e-6,eta=0.04,beta=0.5)
+    _, gda, _, _ = gradient_descent_adaptive_step(x0,f,g,step_size=0.09,niter=1000,tol=1e-6)
+    _, rms, _ = rmsprop(x0,f,g,niter=1000,tol=1e-6,eta=0.9,gamma=0.9)
+    _, ada, _ = adam(x0,f,g,niter=1000,tol=1e-6,eta=0.9,beta1=0.1,beta2=0.9)
     
-    plot_sequence(f,gd1,[-5,-5],[5,5],0.1,30)
+    
+    #plot_sequence(f,gd1,[-5,-5],[5,5],0.1,30)
     plot_sequence(f,gd2,[-5,-5],[5,5],0.1,30)
-    plot_sequence(f,gda,[-5,-5],[5,5],0.1,30)
+    plot_sequence(f,gdm,[-5,-5],[5,5],0.1,30)
+    plot_sequence(f,rms,[-5,-5],[5,5],0.1,30)
+    plot_sequence(f,ada,[-5,-5],[5,5],0.1,30)
+    
+    print(f'niter: \n gd1 {len(gd1)} \n gd2 {len(gd2)} \n gda {len(gda)} \n gdm {len(gdm)} \n rms {len(rms)} \n rms {len(ada)}')
+    
     plt.show()
+
+
     #plt.plot(ys)
     #plt.show()
     #plt.plot(ss)
