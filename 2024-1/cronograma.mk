e800d85aaea8a702684fd9ce61d5cb89ea903f69
rcpsilva <rcpsilva@gmail.com>
2024-01-25 14:31:07 -0300

    added cronograma 2024-1

---
+++ 2024-1/cronograma.mk
@@ -0,0 +1,21 @@
+## Cronograma
+
+| Semana             | Conteúdo                                     |
+| ------------------ | -------------------------------------------- |
+| 25/03/2024 à 27/03/2024 | Introdução e motivação          		   |
+| 01/04/2024 à 03/04/2024 | Revisão de conceitos matemáticos           |
+| 08/04/2024 à 10/04/2024 | Máximo e míminos de funcões com várias variáveis |
+| 15/04/2024 à 17/04/2024 | Máximo e míminos de funcões com várias variáveis |
+| 22/04/2024 à 24/04/2024 | Questões de convexidade e Globalidade     |
+| 29/04/2024              | Apresentações dos temas dos trabalhos     |
+| 06/05/2024 à 08/05/2024 | Algoritmos de descida                     |
+| 13/05/2024 à 15/05/2024 | Métodos de primeira ordem (I)             |
+| 20/05/2024 à 22/05/2024 | Variações modernas do método do gradiente |
+| 27/05/2024 à 29/05/2024 | Métodos de primeira ordem (II)            |
+| 03/06/2024 à 05/06/2024 | Método de segunda ordem                   |
+| 10/06/2024 à 12/06/2024 | Método de quase Newton                    |
+| 17/06/2024 à 19/06/2024 | Otimização com restrições                 |
+| 24/06/2024 à 26/06/2024 | Otimização com restrições                 |
+| 01/07/2024 à 03/07/2024 | Otimização com restrições                 |
+| 10/07/2024 e 15/07/2024 | Apresentações dos trabalhos               |
+| 22/07/2024              | Exame Especial                            |
