9e62aaafc9c247898d5c436efd9e3047604ca488
rcpsilva <rcpsilva@gmail.com>
2024-01-25 15:23:39 -0300

    Update cronograma.mk

--- 2024-1/cronograma.mk
+++ 2024-1/cronograma.mk
@@ -4,8 +4,8 @@
 | ------------------ | -------------------------------------------- |
 | 25/03/2024 à 27/03/2024 | Introdução e motivação          		   |
 | 01/04/2024 à 03/04/2024 | Revisão de conceitos matemáticos           |
-| 08/04/2024 à 10/04/2024 | Máximo e míminos de funcões com várias variáveis |
-| 15/04/2024 à 17/04/2024 | Máximo e míminos de funcões com várias variáveis |
+| 08/04/2024 à 10/04/2024 | Máximos e míminos de funcões com várias variáveis |
+| 15/04/2024 à 17/04/2024 | Máximos e míminos de funcões com várias variáveis |
 | 22/04/2024 à 24/04/2024 | Questões de convexidade e Globalidade     |
 | 29/04/2024              | Apresentações dos temas dos trabalhos     |
 | 06/05/2024 à 08/05/2024 | Algoritmos de descida                     |
@@ -19,3 +19,41 @@
 | 01/07/2024 à 03/07/2024 | Otimização com restrições                 |
 | 10/07/2024 e 15/07/2024 | Apresentações dos trabalhos               |
 | 22/07/2024              | Exame Especial                            |
+
+
+Metodologia:
+
+Aulas expositivas sobre o conteúdo programático
+
+Estudos Dirigidos: atividades individuais práticas contendo exercícios e implementações dos métodos estudados. Serão distribuidos e avaliados durante as aulas.
+
+Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Observações: A principal linguagem de programação deste curso será a linguagem Python. O código fonte dos trabalhos práticos será submetido pelo GitHub. O aluno precisará ter acesso à internet e um computador (desktop ou laptop).
+
+Atividades avaliativas:
+
+Estudos dirigidos (EDs) de 10 pontos
+
+1 Trabalho Prático dividido em duas estapas: (i) apresentação do tema (T1) - 10 pontos e (ii) Apresentção do desenvolvimento e resultados (T2) - 10 pontos
+
+Nota Final = (0.1 x T1 + 0.5 x T2 + 0.4 x média(EDs))/10,0
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Bibliografia
+
+BAZARAA, M. S.; SHERALI, H. D.; SHETTY, C. M. Nonlinear programming: Theory and algorithms. 3rd ed. Hoboken, N.J.: Wiley-Interscience, 2006.
+
+TAVARES, L. V.; CORREIA, F. N. Optimização linear e não linear: conceitos, métodos e algoritmos. 2. ed. Lisboa: Fundação Calouste Gulbenkian, 1999.
+
+BEVERIDGE, G. S. G.; SCHECHTER, R. S. Optimization: theory and practice. Tóquio, Auckland, Düsseldorf, Joanesburgo, Londres, México, Nova Deli, Panamá, São Paulo, Cingapura, Sydney: McGraw-Hill Kogakusha, Ltda, 1970.
+
+
+IZMAILOV, A.; SOLODOV, M. Otimização, volume 2: métodos computacionais. Rio de Janeiro: IMPA, 2007.
+
+MATEUS, G. R.; LUNA, H. P. L. Programação não linear. Belo Horizonte: UFMG, 1986. 
+
+BALAKRISHNAN, A. V. (Ed.). Techniques of optimization. New York: Academic Press,
+1972.
+
