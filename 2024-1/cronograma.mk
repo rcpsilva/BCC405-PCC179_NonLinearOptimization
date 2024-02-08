ecd266dd610299b70da95269a4af33c654e37e33
rcpsilva <rcpsilva@gmail.com>
2024-02-08 17:19:32 -0300

    Update cronograma.mk

--- 2024-1/cronograma.mk
+++ 2024-1/cronograma.mk
@@ -1,24 +1,24 @@
 ## Cronograma
 
-| Semana             | Conteúdo                                     |
-| ------------------ | -------------------------------------------- |
-| 25/03/2024 à 27/03/2024 | Introdução e motivação          		   |
-| 01/04/2024 à 03/04/2024 | Revisão de conceitos matemáticos           |
-| 08/04/2024 à 10/04/2024 | Máximos e míminos de funcões com várias variáveis |
-| 15/04/2024 à 17/04/2024 | Máximos e míminos de funcões com várias variáveis |
-| 22/04/2024 à 24/04/2024 | Questões de convexidade e Globalidade     |
-| 29/04/2024              | Apresentações dos temas dos trabalhos     |
-| 06/05/2024 à 08/05/2024 | Algoritmos de descida                     |
-| 13/05/2024 à 15/05/2024 | Métodos de primeira ordem (I)             |
-| 20/05/2024 à 22/05/2024 | Variações modernas do método do gradiente |
-| 27/05/2024 à 29/05/2024 | Métodos de primeira ordem (II)            |
-| 03/06/2024 à 05/06/2024 | Método de segunda ordem                   |
-| 10/06/2024 à 12/06/2024 | Método de quase Newton                    |
-| 17/06/2024 à 19/06/2024 | Otimização com restrições                 |
-| 24/06/2024 à 26/06/2024 | Otimização com restrições                 |
-| 01/07/2024 à 03/07/2024 | Otimização com restrições                 |
-| 10/07/2024 e 15/07/2024 | Apresentações dos trabalhos               |
-| 22/07/2024              | Exame Especial                            |
+ Semana             & Conteúdo                                     
+ ------				& --------
+ 25/03/2024 à 27/03/2024 & Introdução e motivação          		   
+ 01/04/2024 à 03/04/2024 & Revisão de conceitos matemáticos           
+ 08/04/2024 à 10/04/2024 & Máximos e míminos de funcões com várias variáveis 
+ 15/04/2024 à 17/04/2024 & Máximos e míminos de funcões com várias variáveis 
+ 22/04/2024 à 24/04/2024 & Questões de convexidade e Globalidade     
+ 29/04/2024              & Apresentações dos temas dos trabalhos     
+ 06/05/2024 à 08/05/2024 & Algoritmos de descida                     
+ 13/05/2024 à 15/05/2024 & Métodos de primeira ordem (I)             
+ 20/05/2024 à 22/05/2024 & Variações modernas do método do gradiente 
+ 27/05/2024 à 29/05/2024 & Métodos de primeira ordem (II)            
+ 03/06/2024 à 05/06/2024 & Método de segunda ordem                   
+ 10/06/2024 à 12/06/2024 & Método de quase Newton                    
+ 17/06/2024 à 19/06/2024 & Otimização com restrições                 
+ 24/06/2024 à 26/06/2024 & Otimização com restrições                 
+ 01/07/2024 à 03/07/2024 & Otimização com restrições                 
+ 10/07/2024 e 15/07/2024 & Apresentações dos trabalhos               
+ 22/07/2024              & Exame Especial                            
 
 
 Metodologia:
@@ -43,17 +43,22 @@ Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo par
 
 Bibliografia
 
-BAZARAA, M. S.; SHERALI, H. D.; SHETTY, C. M. Nonlinear programming: Theory and algorithms. 3rd ed. Hoboken, N.J.: Wiley-Interscience, 2006.
+RIBEIRO, Ademir Alves; KARAS, Elizabeth Wegner. Otimização Contínua: aspectos teóricos e computacionais. São Paulo: Cengage Learning, 2013.
 
-TAVARES, L. V.; CORREIA, F. N. Optimização linear e não linear: conceitos, métodos e algoritmos. 2. ed. Lisboa: Fundação Calouste Gulbenkian, 1999.
+ZÖRNIG, Peter. Introdução à programação não linear. Brasília: UNB, 2011.
 
-BEVERIDGE, G. S. G.; SCHECHTER, R. S. Optimization: theory and practice. Tóquio, Auckland, Düsseldorf, Joanesburgo, Londres, México, Nova Deli, Panamá, São Paulo, Cingapura, Sydney: McGraw-Hill Kogakusha, Ltda, 1970.
+BORTOLOSSI, Humberto José. Cálculo diferencial a várias variáveis. São Paulo: Edições Loyola, 2002.
 
+
+Complementar
 
 IZMAILOV, A.; SOLODOV, M. Otimização, volume 2: métodos computacionais. Rio de Janeiro: IMPA, 2007.
 
 MATEUS, G. R.; LUNA, H. P. L. Programação não linear. Belo Horizonte: UFMG, 1986. 
 
-BALAKRISHNAN, A. V. (Ed.). Techniques of optimization. New York: Academic Press,
-1972.
+BAZARAA, M. S.; SHERALI, H. D.; SHETTY, C. M. Nonlinear programming: Theory and algorithms. 3rd ed. Hoboken, N.J.: Wiley-Interscience, 2006.
+
+TAVARES, L. V.; CORREIA, F. N. Optimização linear e não linear: conceitos, métodos e algoritmos. 2. ed. Lisboa: Fundação Calouste Gulbenkian, 1999.
+
+BEVERIDGE, G. S. G.; SCHECHTER, R. S. Optimization: theory and practice. Tóquio, Auckland, Düsseldorf, Joanesburgo, Londres, México, Nova Deli, Panamá, São Paulo, Cingapura, Sydney: McGraw-Hill Kogakusha, Ltda, 1970.
 
