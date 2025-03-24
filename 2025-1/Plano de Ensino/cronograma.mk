b32b45f9e7832c1de0bde3cadd049173ca2d0ce1
rcpsilva <rcpsilva@gmail.com>
2025-03-24 13:34:18 -0300

    updated cronograma

---
+++ 2025-1/Plano de Ensino/cronograma.mk
@@ -0,0 +1,64 @@
+## Cronograma
+
+ Semana             & Conteúdo                                     
+ ------				& --------
+ 1                  & Introdução e motivação          		   
+ 2                  & Revisão de conceitos matemáticos           
+ 3                  & Máximos e míminos de funcões com várias variáveis 
+ 4                  & Máximos e mínimos de funções com várias variáveis  
+ 5                  & Questões de convexidade e Globalidade  
+ 6                  & Apresentações dos temas dos trabalhos  
+ 7                  & Algoritmos de descida  
+ 8                  & Métodos de primeira ordem (I)  
+ 9                  & Variações modernas do método do gradiente  
+ 10                 & Métodos de primeira ordem (II)  
+ 11                 & Método de segunda ordem  
+ 12                 & Método de quasi Newton  
+ 13                 & Otimização com restrições  
+ 14                 & Otimização com restrições  
+ 15                 & Apresentações dos trabalhos  
+ 16                 & Apresentações dos trabalhos
+ 17                 & Apresentações dos trabalhos  
+ 01 de Setembro     & Exame Especial
+
+Metodologia:
+
+Aulas expositivas sobre o conteúdo programático
+
+Estudos Dirigidos: atividades individuais práticas contendo exercícios e implementações dos métodos estudados. Serão distribuidos e avaliados durante as aulas.
+
+Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Observações: A principal linguagem de programação deste curso será a linguagem Python. O código fonte dos trabalhos práticos será submetido pelo GitHub. O aluno precisará ter acesso à internet e um computador (desktop ou laptop).
+
+Atividades avaliativas:
+
+Estudos dirigidos (EDs) de 10 pontos
+
+Trabalho Prático (T) 
+
+Nota Final = (0.6 x T + 0.4 x média(EDs))/10,0
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Bibliografia
+
+RIBEIRO, Ademir Alves; KARAS, Elizabeth Wegner. Otimização Contínua: aspectos teóricos e computacionais. São Paulo: Cengage Learning, 2013.
+
+ZÖRNIG, Peter. Introdução à programação não linear. Brasília: UNB, 2011.
+
+BORTOLOSSI, Humberto José. Cálculo diferencial a várias variáveis. São Paulo: Edições Loyola, 2002.
+
+
+Complementar
+
+IZMAILOV, A.; SOLODOV, M. Otimização, volume 2: métodos computacionais. Rio de Janeiro: IMPA, 2007.
+
+MATEUS, G. R.; LUNA, H. P. L. Programação não linear. Belo Horizonte: UFMG, 1986. 
+
+BAZARAA, M. S.; SHERALI, H. D.; SHETTY, C. M. Nonlinear programming: Theory and algorithms. 3rd ed. Hoboken, N.J.: Wiley-Interscience, 2006.
+
+TAVARES, L. V.; CORREIA, F. N. Optimização linear e não linear: conceitos, métodos e algoritmos. 2. ed. Lisboa: Fundação Calouste Gulbenkian, 1999.
+
+BEVERIDGE, G. S. G.; SCHECHTER, R. S. Optimization: theory and practice. Tóquio, Auckland, Düsseldorf, Joanesburgo, Londres, México, Nova Deli, Panamá, São Paulo, Cingapura, Sydney: McGraw-Hill Kogakusha, Ltda, 1970.
+
