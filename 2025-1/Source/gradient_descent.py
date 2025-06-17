7f73efc33562f7a06f973b4858811f645f04ad1d
rcpsilva <rcpsilva@gmail.com>
2025-06-17 19:54:37 -0300

    Update gradient_descent.py

--- 2025-1/Source/gradient_descent.py
+++ 2025-1/Source/gradient_descent.py
@@ -5,27 +5,59 @@ def gradient_descent(x0,f,gf,eta,i_max,tol,verbose=True):
     i = 0
     x = x0
     while (i < i_max) and (np.linalg.norm(gf(x)) > tol):
+        
+        eta = find_eta_armijo(f,x,gf,-gf(x))
+        
         x = x - eta * gf(x)
         i += 1
 
         if verbose:
             print(f'{i}: \t x: {x} \t ||g||: {np.linalg.norm(gf(x)):.6f} \t f(x): {f(x):.4f}')
 
-def example1():
+def find_eta_armijo(f,x,gf,d):
 
-    f = lambda x : x[0]**2 + x[1]**2 + 20
+    gamma = 0.9
+    eta = 0.5
+
+    l = 1
+
+    while f(x + l*d) > f(x) + eta*l*np.dot(gf(x),d.T):
+        l = gamma*l
+
+    return l
+    
+
+def newton(x0,f,gf,H,eta,i_max,tol,verbose=True):
+
+    i = 0 
+    x = x0
+    while (i < i_max) and (np.linalg.norm(gf(x)) > tol):
+        x = x - eta * np.dot(np.linalg.inv(H(x)),gf(x).T)
+        i += 1
+
+        if verbose:
+            print(f'{i}: \t x: {x} \t ||g||: {np.linalg.norm(gf(x)):.6f} \t f(x): {f(x):.4f}')
+
+
+def example1(method):
+
+    f = lambda x : x[0]**2 + x[1]**2 + x[0]*x[1] + 20
     gf = lambda x: np.array([2*x[0], 2*x[1]])
+    H = lambda x: np.array([[2,0],
+                            [0,2]])
     eta = 0.05
     i_max = 1000
     tol = 1e-6
     x0 = np.array([-5,5])
-    return [x0,f,gf,eta,i_max,tol]
+    return [x0,f,gf,eta,i_max,tol] if method == 'gradient' else [x0,f,gf,H,eta,i_max,tol]
 
 if __name__ == '__main__':
     
-    params = example1()
+    params = example1('gradient')
     gradient_descent(*params)
 
+    params = example1('newton')
+    newton(*params)
 
 
     
