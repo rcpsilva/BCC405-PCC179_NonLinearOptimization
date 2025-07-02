a6cd3c92c4025a5f6377e7b4d688e4fb563f9abc
rcpsilva <rcpsilva@gmail.com>
2025-07-02 18:38:25 -0300

    added optimizers

--- 2025-1/Source/experiment.py
+++ 2025-1/Source/experiment.py
@@ -1,4 +1,4 @@
-from optimizers import gradient_descent
+from optimizers import gradient_descent, gradient_cristiano, gradient_adaptive, gradient_momentum
 from visualize import plot_sequence, function_contour
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,17 +6,23 @@ import matplotlib.pyplot as plt
 f = lambda x: 0.1*x[0]**2 + 10*x[1]**2
 g = lambda x: np.array([0.2*x[0], 20*x[1]]) #0.09
 
-f = lambda x: 2*x[0]**2 + x[1]**2 + 2*x[0]*x[1]
-g = lambda x: np.array([4*x[0] + 2*x[1], 2*x[1] + 2*x[0]]) #0.03
+#f = lambda x: 2*x[0]**2 + x[1]**2 + 2*x[0]*x[1]
+#g = lambda x: np.array([4*x[0] + 2*x[1], 2*x[1] + 2*x[0]]) #0.03
 
 x0 = np.array([-4.5,-4.5])
 
-_, gd1, _, _ = gradient_descent(x0,f,g,step_size=0.3,niter=1000,tol=1e-6)
+_, gd1, _, _ = gradient_descent(x0,f,g,step_size=0.09,niter=1000,tol=1e-6)
+#_, gc1, _, _ = gradient_cristiano(x0,f,g,step_size=0.05,niter=1000,tol=1e-6)
+#_, ga1, _, _ = gradient_adaptive(x0,f,g,step_size=0.5,niter=1000,tol=1e-6)
+_, gm1, _, _ = gradient_momentum(x0,f,g,step_size=0.1,niter=1000,tol=1e-6,beta=0.5)
 
 ax = function_contour(f,[-5,-5],[5,5],0.1,30)
 
 plot_sequence(f,gd1,ax)
+#plot_sequence(f,gc1,ax)
+#plot_sequence(f,ga1,ax)
+plot_sequence(f,gm1,ax)
 
-print(f'niter: \n gd1 {len(gd1)}')
+print(f'niter: \n gd1 {len(gd1)} \n gm1 {len(gm1)}')
 
 plt.show()
--- 2025-1/Source/optimizers.py
+++ 2025-1/Source/optimizers.py
@@ -1,5 +1,29 @@
 import numpy as np
 
+
+def gradient_momentum(x0,f,grad,step_size=0.9,niter=500,tol=1e-6,beta=0.98):
+
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    ss = [step_size]
+    s = step_size
+    v = np.zeros_like(x)
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        d = grad(x)
+        v = beta*v + (1-beta)*d
+        x = x - s*v
+        ss.append(s)
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d}')
+        iter+=1
+        s = step_size
+        
+
+    return x, xs, ys, ss
+
 def gradient_descent(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
 
     x = x0
@@ -19,4 +43,59 @@ def gradient_descent(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
         s = step_size
         
 
-    return x, xs, ys,ss
+    return x, xs, ys, ss
+
+def gradient_adaptive(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
+
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    ss = [step_size]
+    s = step_size
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        d = -grad(x)
+        s = find_eta_armijo(f,x,grad,d)
+        x = x + s*d
+        ss.append(s)
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d}')
+        iter+=1
+        s = step_size
+        
+
+    return x, xs, ys, ss
+
+def find_eta_armijo(f,x,gf,d):
+
+    gamma = 0.9
+    eta = 0.5
+
+    l = 1
+
+    while f(x + l*d) > f(x) + eta*l*np.dot(gf(x),d.T):
+        l = gamma*l
+
+    return l
+
+def gradient_cristiano(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
+
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    ss = [step_size]
+    s = step_size
+    increment = 0.005
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        d = -grad(x)
+        x = x + s*d
+        ss.append(s)
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d}')
+        iter+=1
+        s = s + increment
+        
+    return x, xs, ys, ss
