8016842acb638ef9679defe43b9a79ab16390bcd
rcpsilva <rcpsilva@gmail.com>
2025-07-15 14:35:55 -0300

    added benchmark functions

--- 2025-1/Source/experiment.py
+++ 2025-1/Source/experiment.py
@@ -1,28 +1,63 @@
-from optimizers import gradient_descent, gradient_cristiano, gradient_adaptive, gradient_momentum
+from optimizers import gradient_descent, gradient_cristiano, gradient_adaptive, gradient_momentum, RMSProp, Adam
 from visualize import plot_sequence, function_contour
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = lambda x: 0.1*x[0]**2 + 10*x[1]**2
-g = lambda x: np.array([0.2*x[0], 20*x[1]]) #0.09
 
-#f = lambda x: 2*x[0]**2 + x[1]**2 + 2*x[0]*x[1]
-#g = lambda x: np.array([4*x[0] + 2*x[1], 2*x[1] + 2*x[0]]) #0.03
+fs = []
+gs = []
+
+
+fs.append(lambda x: 0.1*x[0]**2 + 10*x[1]**2)
+gs.append(lambda x: np.array([0.2*x[0], 20*x[1]])) #0.09
+
+fs.append(lambda x: 0.1*(x[0]-1)**2 + 10*(x[1]-2)**2)
+gs.append(lambda x: np.array([0.2*(x[0]-1), 20*(x[1]-2)]))
+
+
+fs.append(lambda x: 2*x[0]**2 + x[1]**2 + 2*x[0]*x[1])
+gs.append(lambda x: np.array([4*x[0] + 2*x[1], 2*x[1] + 2*x[0]])) #0.03
+
+# Função de Rosenbrock (2 variáveis)
+fs.append(lambda x: 100.0 * (x[1] - x[0]**2)**2 + (1.0 - x[0])**2)
+
+# Gradiente da Rosenbrock
+gs.append(lambda x: np.array([
+    -400.0 * (x[1] - x[0]**2) * x[0] - 2.0 * (1.0 - x[0]),  # ∂f/∂x₁
+     200.0 * (x[1] - x[0]**2)                               # ∂f/∂x₂
+]))
+
+# Função de Booth (mínimo único em (1, 3) com f = 0)
+fs.append(lambda x: (x[0] + 2.0 * x[1] - 7.0)**2 + (2.0 * x[0] + x[1] - 5.0)**2)
+
+# Gradiente da Booth
+gs.append(lambda x: np.array([
+    10.0 * x[0] +  8.0 * x[1] - 34.0,   # ∂f/∂x₁
+     8.0 * x[0] + 10.0 * x[1] - 38.0    # ∂f/∂x₂
+]))
 
 x0 = np.array([-4.5,-4.5])
+prob_idx = 3
+niter = 5000
 
-_, gd1, _, _ = gradient_descent(x0,f,g,step_size=0.09,niter=1000,tol=1e-6)
-#_, gc1, _, _ = gradient_cristiano(x0,f,g,step_size=0.05,niter=1000,tol=1e-6)
-#_, ga1, _, _ = gradient_adaptive(x0,f,g,step_size=0.5,niter=1000,tol=1e-6)
-_, gm1, _, _ = gradient_momentum(x0,f,g,step_size=0.1,niter=1000,tol=1e-6,beta=0.5)
+_, gd1, f_gd1, _ = gradient_descent(x0,fs[prob_idx],gs[prob_idx],step_size=1e-10,niter=niter,tol=1e-6)
+#_, gc1, _, _ = gradient_cristiano(x0,fs[prob_idx],gs[prob_idx],step_size=0.05,niter=niter,tol=1e-6)
+_, ga1, f_ga1, _ = gradient_adaptive(x0,fs[prob_idx],gs[prob_idx],step_size=0.9,niter=niter,tol=1e-6)
+#_, gm1, _, _ = gradient_momentum(x0,f,g,step_size=0.1,niter=niter,tol=1e-6,beta=0.9)
+#_, gm1, f_gm1, _ = gradient_momentum(x0,fs[prob_idx],gs[prob_idx],step_size=0.9,niter=niter,tol=1e-6,beta=0.9)
+_, rms, f_rms, _ = RMSProp(x0,fs[prob_idx],gs[prob_idx],step_size=0.9,niter=niter,tol=1e-6,beta=0.99)
+_, ada, f_ada, _ = Adam(x0,fs[prob_idx],gs[prob_idx],step_size=0.9,niter=niter,tol=1e-6,beta1=0.9,beta2=0.99)
 
-ax = function_contour(f,[-5,-5],[5,5],0.1,30)
+res = [(ga1,f_ga1),(rms,f_rms),(ada, f_ada)]
 
-plot_sequence(f,gd1,ax)
-#plot_sequence(f,gc1,ax)
-#plot_sequence(f,ga1,ax)
-plot_sequence(f,gm1,ax)
+#res = [(ga1,f_ga1)]
 
-print(f'niter: \n gd1 {len(gd1)} \n gm1 {len(gm1)}')
+ax = function_contour(fs[prob_idx],[-5,-5],[5,5],0.1,30)
+
+for r in res:
+    plot_sequence(fs[prob_idx],r[0],ax)
+
+for r in res:
+    print(f'niter: {len(r[0])} best: {r[1][-1]}')
 
 plt.show()
--- 2025-1/Source/optimizers.py
+++ 2025-1/Source/optimizers.py
@@ -1,5 +1,54 @@
 import numpy as np
 
+def Adam(x0,f,grad,step_size=0.9,niter=500,tol=1e-6,beta1=0.9,beta2=0.99):
+    eps = 1e-10
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    ss = [step_size]
+    s = step_size
+    z = grad(x0)**2 #np.zeros_like(x)
+    v = grad(x0)
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        d = grad(x)
+        v = beta1*v + (1 - beta1)*d
+        z = beta2*z + (1 - beta2)*d**2 
+        v_hat = v/(1 - beta1**(iter+1))
+        z_hat = z/(1- beta2**(iter+1))
+        x = x - s*(v_hat/(np.sqrt(z_hat) + eps))
+        ss.append(s)
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{v_hat}')
+        iter+=1
+        s = step_size
+        
+    return x, xs, ys, ss
+
+
+def RMSProp(x0,f,grad,step_size=0.9,niter=500,tol=1e-6,beta=0.98):
+    eps = 1e-10
+    x = x0
+    iter = 0
+    xs = [x0]
+    ys = [f(x0)]
+    ss = [step_size]
+    s = step_size
+    z = grad(x0)**2 #np.zeros_like(x)
+    while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
+        d = grad(x)
+        z = beta*z + (1 - beta)*d**2 
+        x = x - s*(d/(np.sqrt(z) + eps))
+        ss.append(s)
+        xs.append(x)
+        ys.append(f(x))
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d}')
+        iter+=1
+        s = step_size
+        
+    return x, xs, ys, ss
+
 
 def gradient_momentum(x0,f,grad,step_size=0.9,niter=500,tol=1e-6,beta=0.98):
 
@@ -55,12 +104,12 @@ def gradient_adaptive(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
     s = step_size
     while (iter < niter) and (np.linalg.norm(grad(x),2) > tol):
         d = -grad(x)
-        s = find_eta_armijo(f,x,grad,d)
+        s,count = find_eta_armijo(f,x,grad,d)
         x = x + s*d
         ss.append(s)
         xs.append(x)
         ys.append(f(x))
-        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d}')
+        print(f'[{iter}] f: {f(x):.4f} g: {np.linalg.norm(grad(x),2):.4f} d:{d} count:{count}')
         iter+=1
         s = step_size
         
@@ -73,11 +122,13 @@ def find_eta_armijo(f,x,gf,d):
     eta = 0.5
 
     l = 1
+    count = 0
 
     while f(x + l*d) > f(x) + eta*l*np.dot(gf(x),d.T):
         l = gamma*l
+        count+=1
 
-    return l
+    return l,count
 
 def gradient_cristiano(x0,f,grad,step_size,niter=500,tol=1e-6,gamma=0.98):
 
